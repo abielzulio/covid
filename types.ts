80cad1c0cf90e830a011f1ca0f03bd2c99b5af8c
Abiel Zulio M <abielzm@gmail.com>
2022-04-10 16:58:06 +0700

    type: init Country type

---
+++ types.ts
@@ -0,0 +1,25 @@
+export interface Country {
+    updated: number;
+    country: string;
+    countryInfo: CountryInfo;
+    cases: number;
+    todayCases: number;
+    deaths: number;
+    todayDeaths: number;
+    recovered: number;
+    todayRecovered: number;
+    active: number;
+    critical: number;
+    casesPerOneMillion: number;
+    deathsPerOneMillion: number;
+    tests: number;
+    testsPerOneMillion: number;
+    population: number;
+    continent: string;
+    oneCasePerPeople: number;
+    oneDeathPerPeople: number;
+    oneTestPerPeople: number;
+    activePerOneMillion: number;
+    recoveredPerOneMillion: number;
+    criticalPerOneMillion: number;
+}
