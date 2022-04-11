a35cad955751a27b969e361d8838e9e54f73cfdd
Abiel Zulio M <abielzm@gmail.com>
2022-04-11 23:23:10 +0700

    index: add today active, death, and recovered cases section

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,12 +4,24 @@ import Head from 'next/head'
 import { Container, Heading, Box, Flex, Center, Spacer, Text, Link } from '@chakra-ui/react'
 import { Select } from '@chakra-ui/react'
 import { Country } from "../types"
+import { useColorModeValue } from '@chakra-ui/react'
 
 const Home: NextPage<{ countries: Country[] }> = ({ countries }) => {
 
   const defaultValueCountryInfo = {}
   const [countryInfo, setCountryInfo] = useState<Country>(defaultValueCountryInfo as Country)
   const [country, setInputCountry] = useState<string>("worldwide")
+
+  /* Initial state when first load showing worldwide cases */
+  useEffect(() => {
+    fetch("https://disease.sh/v3/covid-19/all")
+      .then((res) => res.json())
+      .then((data) => {
+        setCountryInfo(data)
+      })
+  }, [])
+
+
   const onCountryChange: React.ChangeEventHandler = async (e: ChangeEvent<HTMLInputElement>): Promise<void> => {
     const countryCode = e.target.value
     const url = 
@@ -57,6 +69,60 @@ const Home: NextPage<{ countries: Country[] }> = ({ countries }) => {
             </Select>
           </Box>
         </Flex>
+
+        <Box my={6}>
+          <Flex >
+            <Box
+              maxW={'full'}
+              minW={'100px'}
+              w={'full'}
+              bg={useColorModeValue('white', 'gray.800')}
+              boxShadow={'xl'}
+              rounded={'md'}
+              overflow={'hidden'}>
+                <Box w={'full'} bg={'yellow.400'} h={2} />
+                <Box p={4}>
+                  <Text fontSize={'xs'} fontWeight={'bold'}>ACTIVE</Text>
+                  <Text fontSize={'3xl'} color={'yellow.400'} my={1}>+{countryInfo?.todayCases?.toLocaleString() ?? "-"}</Text>
+                  <Text fontSize={'xs'} fontWeight={'normal'}>{countryInfo?.active?.toLocaleString() ?? "-"}</Text>
+                </Box>
+            </Box>
+            <Spacer mx={"2"} />
+            <Box
+              maxW={'full'}
+              minW={'100px'}
+              w={'full'}
+              bg={useColorModeValue('white', 'gray.800')}
+              boxShadow={'xl'}
+              rounded={'md'}
+              overflow={'hidden'}>
+                <Box w={'full'} bg={'red.400'} h={2} />
+                <Box p={4}>
+                  <Text fontSize={"xs"} fontWeight={"bold"}>DEATH</Text>
+                  <Text fontSize={"3xl"} color={"red.400"} my={1}>+{countryInfo?.todayDeaths?.toLocaleString() ?? "-"}</Text>
+                  <Text fontSize={"xs"} fontWeight={"normal"}>{countryInfo?.deaths?.toLocaleString() ?? "-"}</Text>
+                </Box>
+            </Box>
+            <Spacer mx={"2"} />
+            <Box
+              maxW={'full'}
+              minW={'100px'}
+              w={'full'}
+              bg={useColorModeValue('white', 'gray.800')}
+              boxShadow={'xl'}
+              rounded={'md'}
+              overflow={'hidden'}>
+              <Box w={'full'} bg={'green.400'} h={2} />
+              <Box p={4}>
+                <Text fontSize={"xs"} fontWeight={"bold"} opacity={10}>RECOVERED</Text>
+                <Text fontSize={"3xl"} color={"green.400"} my={1}>+{countryInfo?.todayRecovered?.toLocaleString() ?? "-"}</Text>
+                <Text fontSize={"xs"} fontWeight={"normal"}>{countryInfo?.recovered?.toLocaleString() ?? "-"}</Text>
+              </Box>
+              <Box/>
+            </Box>
+          </Flex>
+        </Box>
+
       </Container>
     </div>
   )
