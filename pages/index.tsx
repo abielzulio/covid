e8589e3dc055f85f4bd9153e646bf83132b358d4
Abiel Zulio M <abielzm@gmail.com>
2022-04-11 22:54:02 +0700

    index: add dynamic country select feature

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,29 @@
 import type { NextPage, GetStaticProps } from 'next'
 import React from 'react'
+import React, { ChangeEvent, useEffect, useState } from 'react'
 import Head from 'next/head'
 import { Container, Heading } from '@chakra-ui/react'
+import { Container, Heading, Box, Flex, Center, Spacer, Text, Link } from '@chakra-ui/react'
 import { Select } from '@chakra-ui/react'
 import { Country } from "../types"
 
 const Home: NextPage<{ countries: Country[] }> = ({ countries }) => {
 
+  const [countryInfo, setCountryInfo] = useState<Country>({})
+  const [country, setInputCountry] = useState<string>("worldwide")
+  const onCountryChange: React.ChangeEventHandler = async (e: ChangeEvent<HTMLInputElement>): Promise<void> => {
+    const countryCode = e.target.value
+    const url = 
+      (countryCode === "worldwide") 
+      ? "https://disease.sh/v3/covid-19/all"
+      : `https://disease.sh/v3/covid-19/countries/${countryCode}`
+    await fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        setInputCountry(countryCode)
+        setCountryInfo(data)
+      })
+  }
   return (
     <div>
       <Head>
@@ -28,6 +45,32 @@ const Home: NextPage<{ countries: Country[] }> = ({ countries }) => {
               )
             })}
           </Select>
+      <Container maxWidth="container.xl" p={6}>
+
+        <Center>
+          <Heading as={'h1'} my={4}>Worldwide COVID-19 Cases Tracker</Heading>
+        </Center>
+
+        <Flex my={4}>
+          <Box>
+            <Heading as={'h3'} size={'md'}>Today Cases</Heading>
+            <Text my={1.5}>{countryInfo?.country ?? "Worldwide"}</Text>
+          </Box>
+          <Spacer />
+          <Box>
+            <Select onChange={onCountryChange}>
+              <option value="worldwide">Worldwide</option>
+              {countries.map((ctr: Country, id: number) => {
+                return (
+                    <option 
+                      key={id} 
+                      value={ctr.countryInfo?.iso2}
+                    >{ctr.country}</option>
+                )
+              })}
+            </Select>
+          </Box>
+        </Flex>
       </Container>
     </div>
   )
